7cc71c8bc933f3e3e9c39be65f2af2925ce6c780
John <johnp.dev131@gmail.com>
2023-08-24 22:32:41 +0800

    add-view-pages

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::middleware(['auth'])->group(function () {
@@ -27,6 +25,14 @@ Route::middleware(['auth'])->group(function () {
 });
 
 Auth::routes();
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
 
 
 
