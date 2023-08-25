84d5bdaf3c3b86f0b097e2bb0907346117a946eb
daisy.imbing@gmail.com <email>
2023-08-25 17:45:04 +0800

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,26 @@
 
 <div class="container mt-5">
     <div class="row">
-        <div class="col-md-6">
-        <h1>Welcome to Marites' Home Page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, ipsum at rhoncus euismod, magna ante dignissim dolor, nec posuere ex eros vel nisi.</p>
-        <a href="#" class="btn btn-primary">Learn More</a>
+        <div class="col-md-6 d-flex justify-content-end">
+            <img src="images/welcome.jpeg" alt="Marites Voice Center" class="img-fluid">
         </div>
         <div class="col-md-6">
-        <img src="https://via.placeholder.com/400" alt="Placeholder Image" class="img-fluid">
+            <h1 style="font-size: 3em; font-weight: bold;">Welcome to <br>Marites Voice Center</h1>
+            <br><br>
+            <p>Your online space to connect with people in your community and beyond.<br> Here you can:</p>
+            <ul>
+                <li><b>Connect with your neighbors</b><br>Message and comment on posts to stay in touch with the people in your community, regardless of where they live.</li>
+                <br>
+                <li><b>Post about anything you want</b></li>
+                <ul>
+               <li><b>Information about what's happening in your community:</b>Share news about upcoming festivals, new businesses, or anything else that's happening in your area.</li>
+               <li><b>Your passions:</b> Share your hobbies, interests, and talents with others. Tell us about your love of cooking, gardening, hiking, or anything else that you're passionate about.</li>
+               <a href="{{ route('welcomepage') }}" class="btn btn-primary">Learn More</a>
+
+           
         </div>
     </div>
 </div>
+
 
 @endsection
---
+++ resources/views/welcomepage.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container-fluid mt-5">
+    <div class="row">
+        <div class="col-md-12">
+          <h1 style="font-size: 3em; font-weight: bold; text-align: left;">Welcome to Marites Voice Center</h1>
+            <br>
+            <p style="font-size: 1em; font-weight: bold;">Your online space to connect with people in your community and beyond.<br> Here you can:</p>
+            <ul>
+                <li><b>Connect with your neighbors</b><br>Message and comment on posts to stay in touch with the people in your community, regardless of where they live.</li>
+                <br>
+                <li><b>Post about anything you want.</b></li>
+                <ul>
+               <li><b>Information about what's happening in your community:</b> Share news about upcoming festivals, new businesses, or anything else that's happening in your area.</li>
+               <li><b>Your passions:</b> Share your hobbies, interests, and talents with others. Tell us about your love of cooking, gardening, hiking, or anything else that you're passionate about.</li>
+               <li><b>Your stories and insights:</b> Share your personal experiences, from family memories to travel adventures. Let us in on your thoughts and insights on the world around you.</li>
+               <li><b>Your thoughts on how to make your community better:</b> Share your ideas for improving your community, big or small.</li>
+               <li><b>Your art, music, and writing:</b> Share your creative works with the world. Let us see your paintings, photographs, poems, songs, or anything else that you've created.</li>
+            </ul><br>
+            <p>We believe that everyone has a voice, and we want to help you be heard. So what are you waiting for? Come join us at Marites Voice Center and let us help you connect with your community!</p>
+           
+        </div>
+    </div>
+</div>
+
+
+@endsection
