b7bac014c0938fad32004277f14f11bf8da80ba8
daisy.imbing@gmail.com <email>
2023-08-31 04:15:17 +0800

    update about.blade

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,91 +1,67 @@
 @extends('layouts.app')
 @section('content')
 
-<div style="background-color:#002448; background-size: cover;">
-      
-   <div class="container1 py-0 text-center">
-      <img src="images\header.jpg" alt="Marites Voice Center" class="img-fluid object-fit: cover;">
-   
-              
-         <div class="container2" style="padding-left: 40px; padding-right: 40px; margin-left: 40px; margin-right: 40px;">
-            <div style=" background-image:url(images/connect_with_people.jpg); background-repeat: no-repeat; background-position:center;">
-           
-               <h1 style="font-size: 3em; font-weight: bold;"></h1>
-               <h1 class="mvc" style="color:#00409b; font-family: cursive; font-size: 50px; line-height: 95px; font-weight: normal; padding-top:50px;
-               margin-bottom: 200px; margin-top:0px; text-align: center; text-shadow: 0 3px 3px #fff;"><b>Marites Voice Center (MVC)<b></h1><br><br>
+<div style="background-color: white; background-size: cover;">
+ 
+   <!-- <div class="container1 py-0 text-center">
+      <img src="images\header2.jpg" alt="Marites Voice Center" class="img-fluid object-fit: cover;">               -->
+         <h1 class="mvc" style="color:#00409b; font-family: cursive; font-size: 70px; padding-top:20px; font-weight: normal; 
+               margin-top:0px; text-align: center; text-shadow: 0 3px 3px #fff;"><b>Marites Voice Center<b></h1><br><br>
             </div>
-         <!-- <div style='background-color:antiquewhite">    -->
-               <div class="container3 img-fluid object-fit: cover" style=" background-color:#d2d2d2; padding-left: 30px; padding-right: 40px; padding-top:20px; padding-bottom: 20px; margin-bottom:0; color:#000 ; font-family: 'Helvetica Neue', sans-serif; font-size: 18px; line-height: 28px; text-align: justify;">
-                  <div style="word-wrap: break-word; width: 400px">
-                     <div class="about">
-                     <h2><b>About</b></h2>
-                      
-                     <p>Marites Voice Center is a website application that allows users to send messages, receive messages, comment, and post.</p>
-                     <p>The website is designed to be a convenient and easy-to-use way for users to stay connected with each other.</p>
-                     
-                  </div><br><br>
-                  <div>
-                  <h2><b>Features</b></h2>
-                     <ul style="list-style-type: square">
-                        <li>Create an account</li>
-                        <li>Send private messages</li>
-                        <li>Post public messages</li>
-                        <li>Comment on posts</li>
-                        <li>Search for users and messages</li>
-                        <li>Follow other users</li>
-                        <li>Receive notifications</li>
-                     </ul><br><br>
-         
-                  <h2><b>Benefits</b></h2>
-                     <ul style="list-style-type: square">
-                     <li>Stay connected with friends and family</li>
-                     <li>Share information</li>
-                     <li>Build relationships</li>
-                     <li>Get help and support</li>
-                     <li>Find new people to connect with</li>
-                     </ul><br><br>
-                  </div> 
-                  
-                  <div style="word-wrap: break-word; width: 400px">
-                     <h2><b>Target Audience</b></h2>
-                     <p>Anyone who wants to stay connected with others or share information can use Marites Voice Center. The website is especially useful for people who are:</p>
-                     <ul style="list-style-type: square">
-                        <li>New to a city or country</li>
-                        <li>Looking for friends or support groups</li>
-                        <li>Want to stay in touch with family and friends who live far away</li>
-                        <li>Want to connect with people who share their interests</li>
-                     </ul>
-                  </div><br><br>
-                  <div style="word-wrap: break-word; width: 400px">
-                     <h2><b>Team</b></h2>
-                     <p>Marites Voice Center is created by a team of developers, designers, and content creators who are passionate about building a community where people can connect and share.</p>
-                  </div><br>
-                  <div style="word-wrap: break-word; width: 400px">
-                     <h2><b>Contact Us</b></h2>
-                  <p>If you have any questions or feedback about Marites Voice Center, please contact us at marites@email.com</p>
-            
-               </div>           
-            </div>
+      <div class="container">
+      <div class="row">
+         <div class="col-md-6">
+            <h1>About Marites Voice Center</h1>
+            <p>Marites Voice Center is a website application that allows users to send messages, receive messages, comment, and post. The website is designed to be a convenient and easy-to-use way for users to stay connected with each other.</p>
+            <p>We believe that everyone deserves to have a voice, and we are committed to providing a platform where people can share their stories, connect with others, and build community.</p>
+         </div>
+         <div class="col-md-6">
+            <img src="images/header2.jpg" alt="Marites Voice Center" class="img-fluid">
          </div>
       </div>
-   </div>  
-   <footer>
-      <div class="copyright" style="color:darkgrey">            
-            <div class="col-md-12 text-center">
-               <div><br><br><br>
-               <p class="copyright"style="margin:0%;">Copyright &copy; 2023 Marites Voice Center.</p>
-               </div>
-            </div>
+      <div class="row">
+         <div class="col-md-12">
+            <h2>Our Mission</h2>
+            <p>Our mission is to create a more connected world. We believe that by connecting people, we can make the world a better place.</p>
+            <p>We do this by providing a platform where people can:</p>
+            <ul>
+            <li>Share their stories</li>
+            <li>Connect with others</li>
+            <li>Build community</li>
+            </ul>
          </div>
       </div>
-   </footer>                   
-</div>
-                
- </div>  
-         
-
-
+      <div class="row">
+         <div class="col-md-12">
+            <h2>Our Values</h2>
+            <p>Our values are:</p>
+            <ul>
+            <li>Empathy</li>
+            <li>Respect</li>
+            <li>Courage</li>
+            <li>Innovation</li>
+            </ul>
+            <p>We believe that these values are essential to creating a more connected and inclusive world.</p>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-md-12">
+            <h2>Our Team</h2>
+            <p>We are a team of passionate people who are committed to making Marites Voice Center a success. We are diverse in our backgrounds and experiences, but we are united by our common goal of connecting people.</p>
+            <p>We are always looking for new people to join our team. If you are interested in working with us, please visit our website and submit your resume.</p>
+         </div>
+      </div>
+      <div class="row">
+         <div class="col-md-12">
+            <h2>Contact Us</h2>
+            <p>If you have any questions or feedback about Marites Voice Center, please contact us at marites@email.com</p>
+         </div>
+      </div>
+      </div>
 
 @endsection
+
+
+
 
 
